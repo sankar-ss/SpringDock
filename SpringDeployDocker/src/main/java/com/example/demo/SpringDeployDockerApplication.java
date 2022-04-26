56b46f41edc8fe36d690da9479342ee9bfcc1745
Sankar S <sankar_ss@yahoo.com>
2022-04-26 22:11:18 +0530

    Version 2 with log output

--- SpringDeployDocker/src/main/java/com/example/demo/SpringDeployDockerApplication.java
+++ SpringDeployDocker/src/main/java/com/example/demo/SpringDeployDockerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringDeployDockerApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Test Spring Boot from Docker...");
 		SpringApplication.run(SpringDeployDockerApplication.class, args);
 	}
 
