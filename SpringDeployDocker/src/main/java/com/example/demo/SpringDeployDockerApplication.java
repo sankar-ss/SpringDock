081e75b3c9cc027610b16aea6440cd2b49c3500b
Sankar S <sankar_ss@yahoo.com>
2022-05-29 20:25:58 +0530

    For the version 2

--- SpringDeployDocker/src/main/java/com/example/demo/SpringDeployDockerApplication.java
+++ SpringDeployDocker/src/main/java/com/example/demo/SpringDeployDockerApplication.java
@@ -8,6 +8,7 @@ public class SpringDeployDockerApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Test Spring Boot from Docker...");
+		System.out.println("The next line for Version 2.");
 		SpringApplication.run(SpringDeployDockerApplication.class, args);
 	}
 
